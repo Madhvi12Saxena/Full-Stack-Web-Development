245fed6e23dfba15c714437689f1568163d2d629
Madhvi12Saxena <129391081+Madhvi12Saxena@users.noreply.github.com>
2023-07-13 19:17:19 +0530

    Delete CRUD Assignment directory

--- CRUD Assignment/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectToDatabase=async()=>{mongoose.connect(process.env.DB_URL).then((con)=>{
-    console.log("Database Connected Successfully")
-}).catch((err)=>{
-    console.log("Error While Connecting to DB",err.message)
-})}
-
-module.exports ={connectToDatabase};
--- CRUD Assignment/middlewares/registerDataValidate.js
+++
@@ -1,9 +0,0 @@
-exports.registerDataValidate=(req,res,next)=>{
-    const {name,email,password} = req.body;
-
-    if(req.body && name && email && password){
-        next()
-    }else{
-        res.status(404).send({msg:"all input fields are required"})
-    }
-}
