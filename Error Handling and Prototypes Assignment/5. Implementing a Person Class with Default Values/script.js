fc68c81ec6e4ee5019e0951e6321cf6841783589
Madhvi Saxena <ssms122005@gmail.com>
2023-10-18 14:12:32 +0530

    Update script.js

--- Error Handling and Prototypes Assignment/5. Implementing a Person Class with Default Values/script.js
+++ Error Handling and Prototypes Assignment/5. Implementing a Person Class with Default Values/script.js
@@ -10,8 +10,8 @@ class Person {
   }
   
   // Instantiate an instance of the Person class
-  const person1 = new Person("Mithun", 25);
-  console.log(person1.getDetails()); // Output: Name: John Doe, Age: 25
+  const person1 = new Person("Nobita", 15);
+  console.log(person1.getDetails()); 
   
   const person2 = new Person();
   console.log(person2.getDetails()); // Output: Name: Unknown, Age: 0
