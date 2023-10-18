07652d5b19778b5433290d810740bf5fe9977167
Madhvi Saxena <ssms122005@gmail.com>
2023-10-18 14:04:52 +0530

    Update script.js

--- Callback and Promises Assignment/4. Arrange in alphabetical order/script.js
+++ Callback and Promises Assignment/4. Arrange in alphabetical order/script.js
@@ -1,26 +1,19 @@
-// Sample list of books
-const books = [
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-    { title: "Who are you?", author: "George Orwell", year: 1949 },
-    { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 }
-  ];
+function logBookTitlesInAlphabeticalOrder(books, callback) { 
+     const bookTitles = books.map(book => book.title); 
+     const sortedTitles = bookTitles.sort(); 
+     callback(sortedTitles); 
+ } 
   
-  // Callback function to log titles in alphabetical order
-  function logTitles(titles) {
-    const sortedTitles = titles.sort();
-    console.log("Sorted Titles:");
-    sortedTitles.forEach(title => {
-      console.log(title);
-    });
-  }
+ // Example usage: 
+ const books = [ 
+     { title: "1984", author: "George Orwell", year: 1949 }, 
+     { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 }, 
+     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 } 
+ ]; 
   
-  // Function to extract titles from books
-  function getBookTitles(books, callback) {
-    const titles = books.map(book => book.title);
-    callback(titles);
-  }
+ function printBookTitles(titles) { 
+     console.log("Book titles in alphabetical order:"); 
+     titles.forEach(title => console.log(title)); 
+ } 
   
-  // Call the getBookTitles function and pass the callback function
-  getBookTitles(books, logTitles);
-  
+ logBookTitlesInAlphabeticalOrder(books, printBookTitles);
